798a26ed707e32741e4128cbfeeaad031a21bf44
fouroh <38751363+fouroh@users.noreply.github.com>
2018-04-26 10:04:44 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTrm-gDMi4BImD362Tpn6-ZEbs2Rj7JgZht4y-uC5TqYfSopyF9AERzmXVRVCo2PbRQLqATc3uTRV1n/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XL8uRNvLkPqAWPjkq7S-mx482qw3UmZDud-lTi002V0/';
